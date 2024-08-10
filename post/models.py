71888096a15b46ac4af89c8a06965b26b5ff7160
melikaezatvar <melikaezatvar@gmail.com>
2024-08-10 21:20:54 +0330

    phase 1

--- post/models.py
+++ post/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Post(models.Model):
     title = models.CharField(max_length=64)
     description = models.TextField(max_length=1024, null=True)
--- post/views.py
+++ post/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from .forms import CreatePostForm
 # Create your views here.
 
 
 def test(request):
     return render(request, 'test.html')
+
+
+def create_post(request):
+    context = dict()
+    form = CreatePostForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+    context['form'] = form
+
+    return render(request, 'add_posts.html', context)
